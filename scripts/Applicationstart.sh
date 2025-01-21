698ad93da52e590f8daee247f0cea68e0e09015e
Pravin <pravinjagtap2151@gmail.com>
2025-01-21 16:42:06 +0530

    1st commit for dev

--- scripts/Applicationstart.sh
+++ scripts/Applicationstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running Hook: applicationstart.sh"
 cd /home/ubuntu/uni-solution-api
-source /etc/profile
+# source /etc/profile
 # Start PM2
 pm2 start pm2.config.js 2>&1
 if [ $? != "0" ]; then
