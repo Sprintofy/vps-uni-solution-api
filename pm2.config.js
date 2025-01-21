eef166b6c887d387d6f4ded67d4adb5ebdf2d0df
Pravin <pravinjagtap2151@gmail.com>
2025-01-21 16:40:16 +0530

    1st commit for dev

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-    apps : [{
-        name      : 'Pre Trade API',
-        script    : './bin/server.js',
-        node_args : '-r dotenv/config'
-    }],
-}
+    apps: [
+        {
+            name: 'Pre Trade API',            // Name of your application
+            script: './server.ts',            // Entry point (your server.ts file)
+            node_args: '-r ts-node/register -r dotenv/config',  // Use ts-node to run TypeScript and dotenv for environment variables
+            watch: true,                      // Watch for file changes and restart automatically
+            instances: 1,                     // Number of instances (use 'max' to utilize all CPU cores)
+            exec_mode: 'fork',                // Use 'fork' mode (use 'cluster' for load balancing with multiple instances)
+            env: {
+                NODE_ENV: 'development',        // Environment variable for development
+            },
+            env_production: {
+                NODE_ENV: 'production',         // Environment variable for production
+            },
+        },
+    ],
+};
