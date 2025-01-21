e256dc06318ab0ac07fc836c31d7eb6fc4558ed3
Pravin <pravinjagtap2151@gmail.com>
2025-01-21 16:06:52 +0530

    1st commit for dev

---
+++ scripts/AfterInstallHook.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+cd /home/ubuntu/uni-solution-api
+npm install
+npm run build
+# Kill Pm2 Process
+pm2 kill
+echo "Kill All Pm2 Process"
+# Stop all PM2
+# pm2 stop all
+echo "PM2 Stop All Succesfull"
+#pm2 stop all --silent
---
+++ scripts/Applicationstart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Running Hook: applicationstart.sh"
+cd /home/ubuntu/uni-solution-api
+source /etc/profile
+# Start PM2
+pm2 start pm2.config.js 2>&1
+if [ $? != "0" ]; then
+   error_exit "pm2 start unsuccessful"
+else
+   echo "PM2 started";
+fi
+exit 0
