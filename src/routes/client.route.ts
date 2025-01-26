03682674dc58c26355152209ca1d1fb9261683a5
Pravin <pravinjagtap2151@gmail.com>
2025-01-27 00:37:03 +0530

    1st commit for dev

--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -9,9 +9,9 @@ router.post('/import-clients', clientController.import_clients);
 
 router.post('/save-client', clientController.save_client_info);
 
-router.post('/fetch-all', clientController.fetch_all_clients_with_pagination);
+router.post('/fetch-all', celebrate(clientSchema.fetch_active_clients),clientController.fetch_all_clients_with_pagination);
 
-router.get('/fetch-all', clientController.fetch_all_clients);
+router.get('/fetch-all',celebrate(clientSchema.fetch_active_clients), clientController.fetch_all_clients);
 
 router.get('/fetch-city', clientController.fetch_all_cities);
 
--- src/routes/schemas/client.schema.ts
+++ src/routes/schemas/client.schema.ts
@@ -1,20 +1,10 @@
 'use strict';
 import { Joi, Segments } from 'celebrate';
 
-const fileServiceSchema = Joi.object({
-    fields: Joi.object({
-        user_id: Joi.string().required().label("User ID"),
-        organization_id: Joi.string().required().label("Organization ID"),
-    }).required().label("Fields"),
-
-    files: Joi.array()
-        .min(1)
-        .required()
-        .label("Files")
-        .messages({
-            "array.min": "At least one file must be uploaded.",
-            "any.required": "Files are required.",
-        }),
-}).unknown(true).options({ abortEarly: false });
-
-export default {fileServiceSchema};
+export default {
+    fetch_active_clients: {
+        [Segments.QUERY]: Joi.object().keys({
+            organization_id: Joi.number().integer().required(),
+        }).unknown(true).options({ abortEarly: false }) // Disallow extra fields in the body
+    }
+};
