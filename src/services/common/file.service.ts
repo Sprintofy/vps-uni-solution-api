bb01ab93c4a603a8389ef9f7fa8c3eaa704ca0dc
Pravin <pravinjagtap2151@gmail.com>
2025-02-25 19:17:26 +0530

    resend email

--- src/services/common/file.service.ts
+++ src/services/common/file.service.ts
@@ -188,7 +188,7 @@ const uploadZipFileToS3Bucket = async (organization_id:any,body: any) => {
         // Upload the file to the specified "folder" in S3
         const fileStream = fs.createReadStream( body.file_path);
         const result = await awsS3Bucket.uploadFile(fileStream, s3FolderPath, body.file_name);
-        return CONFIGS.AWS.S3.BASE_URL+result.key;
+        return CONFIGS.AWS.S3.BASE_URL+result.Key;
     } catch (error:any) {
         console.error(`Error processing form or uploading file: ${error.message}`);
         throw error;
