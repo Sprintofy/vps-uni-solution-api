9322ac31ba098cc78df121451c2d80ca4d418f1c
Pravin <pravinjagtap2151@gmail.com>
2025-02-26 11:59:48 +0530

    resend email

--- src/services/emailRead.service.ts
+++ src/services/emailRead.service.ts
@@ -828,6 +828,7 @@ function formatDate(dateString: string) {
         hour: "2-digit",
         minute: "2-digit",
         hour12: true,
+        timeZone: "Asia/Kolkata", // Set to IST
     }).format(date);
 }
 
