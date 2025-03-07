d385913287f6dac78c27a6be24567143acb258ef
Pravin <pravinjagtap2151@gmail.com>
2025-03-07 17:42:35 +0530

    resend email

--- src/services/emailRead.service.ts
+++ src/services/emailRead.service.ts
@@ -1015,8 +1015,8 @@ const read_email_proof_wise = async (req: any) => {
         const subject = "Pre Trade Confirmation";
         console.log("results[0].created_date",results[0].created_date)
         let date = moment(results[0].created_date).format('YYYY-MM-DD');// Get today's date
-        const timeIST = moment(results[0].created_date).tz("Asia/Kolkata").subtract(2, 'minutes').format("HH:mm:ss");
-        let startTime = moment(`${date} ${timeIST}`, "YYYY-MM-DD HH:mm","Asia/Kolkata").unix(); // 7:00 AM IST
+        // const timeIST = moment(results[0].created_date).tz("Asia/Kolkata").subtract(2, 'minutes').format("HH:mm:ss");
+        let startTime = moment(`${date} 07:00`, "YYYY-MM-DD HH:mm","Asia/Kolkata").unix(); // 7:00 AM IST
         let endTime = moment(`${date} 23:00`, "YYYY-MM-DD HH:mm","Asia/Kolkata").unix(); // 11:00 PM IST
 
         let query= `{from:${results[0].client_email} to:${results[0].client_email}} subject:"${subject}" after:${startTime} before:${endTime}`
