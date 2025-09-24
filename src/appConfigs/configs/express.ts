89dd1c13816418a7837c629c3134584ce5ed6b1a
Harshad Dhongade <116417528+harshaddhongade2001@users.noreply.github.com>
2025-09-24 13:58:29 +0530

    CORS Fixed

--- src/appConfigs/configs/express.ts
+++ src/appConfigs/configs/express.ts
@@ -17,24 +17,17 @@ const app = express();
 app.options('*', cors());
 
 // Custom headers and cache control middleware
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-//     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-//     res.header('Expires', '-1');
-//     res.header('Pragma', 'no-cache');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token_access, user_id, User-agent");
-//     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token_access, user_id, User-agent");
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 
-// new code (Due to CORS error)
-app.use(cors({
-    origin: 'https://uni-solution.sprintofy.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'token_access', 'user_id', 'User-agent'],
-    credentials: true
-}));
 
 app.use(bodyParser.json());
 
